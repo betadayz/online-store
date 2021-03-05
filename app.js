e7446d3e42bf963f41673866d9cffa1e7f08a69e
BeTa_DaYz <betadayz888@gmail.com>
2021-03-05 15:51:53 +0100

    Product and category model created

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/products');
 
 // db
 mongoose.connect(process.env.DATABASE, {
@@ -30,6 +31,7 @@ app.use(expressValidator());
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 const port = process.env.PORT || 8000;
 
