0b4b23ad0df9f600d116759da1d33806ebd0406d
BeTa_DaYz <betadayz888@gmail.com>
2021-03-08 16:52:42 +0100

    Creating product on the server couple with the product Id

--- controllers/product.js
+++ controllers/product.js
@@ -4,19 +4,54 @@ const fs = require('fs');
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler")
 
+exports.productById = (req, res, next, id) => {
+    Product.findId(id).exec((err, product) => {
+        if(err || !product) {
+            return res.status(400).json({
+                error: 'Product could not be found'
+            });
+        }
+        req.product = product
+        console.log(req.product)
+        next()
+    })
+}
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+};
 
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
-    form.keepExtensions = true
+    form.keepExtensions = true;
     form.parse(req, (err, fields, files) => {
         if(err) {
             return res.status(400).json({
                 error: 'Image could not be uploaded'
             });
         };
-        let product = new Product(fields)
+        const {name, description, price, category, quantity, shipping} = fields
+
+        if(!name || 
+           !description || 
+           !price || 
+           !category || 
+           !quantity || 
+           !shipping) {
+            return res.status(400).json({
+                error: 'Image should be less than 1mb in size'
+            });
+        }
+
+        let product = new Product(fields);
+        
         if(files.photo) {
-            console.log('FILES PHOTO: ', files.photo)
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'All fields are required'
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
@@ -25,7 +60,7 @@ exports.create = (req, res) => {
             if(err) {
                 return res.status(400).json({
                     error: errorHandler(err)
-                })
+                });
             }
             res.json(result);
         })
